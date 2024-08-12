8101d0f9807d71c56db1427d64cad5ec6ae34150
w1shope <wishhopelee@gmail.com>
2024-08-12 22:21:18 +0900

    fix: prod 환경으로 실행하기 위해 쉘스크립트 수정

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -3,5 +3,5 @@
 echo "--------------- 서버 배포 시작 -----------------"
 cd /home/ubuntu/todobuddy-backend
 sudo fuser -k -n tcp 8080 || true
-nohup java -jar project.jar > ./output.log 2>&1 &
+nohup java -jar project.jar --spring.profiles.active=prod > ./output.log 2>&1 &
 echo "--------------- 서버 배포 끝 -----------------"
