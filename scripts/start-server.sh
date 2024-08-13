2249cde9458d3473b3e428ab389fed96a81c7be1
w1shope <wishhopelee@gmail.com>
2024-08-13 16:05:59 +0900

    feat: AWS Param Store로 환경변수 관리

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-export JWT_SECRET_KEY=${JWT_SECRET_KEY}
-export DB_URL=${DB_URL}
-export DB_USERNAME=${DB_USERNAME}
-export DB_PASSWORD=${DB_PASSWORD}
-export MAIL_USERNAME=${MAIL_USERNAME}
-export MAIL_PASSWORD=${MAIL_PASSWORD}
-export REDIS_HOST=${REDIS_HOST}
-export REDIS_PORT=${REDIS_PORT}
-export JWT_EXPIRATION_TIME=${JWT_EXPIRATION_TIME}
-export JWT_ISSUER=${JWT_ISSUER}
-
 echo "--------------- 서버 배포 시작 -----------------"
 cd /home/ubuntu/todobuddy-backend
 sudo fuser -k -n tcp 8080 || true
