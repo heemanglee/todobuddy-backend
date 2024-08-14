2425183d2afab66a629506756b4dfe5d741e95bc
w1shope <wishhopelee@gmail.com>
2024-08-14 11:38:06 +0900

    chore: 컨트롤러 명세서 정보 추가

--- src/main/java/com/todobuddy/backend/controller/MemoController.java
+++ src/main/java/com/todobuddy/backend/controller/MemoController.java
@@ -6,7 +6,11 @@ import com.todobuddy.backend.dto.CreateMemoResponse;
 import com.todobuddy.backend.entity.User;
 import com.todobuddy.backend.security.CurrentUser;
 import com.todobuddy.backend.service.MemoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,10 +23,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/memos")
 @SecurityRequirement(name = "bearerAuth")
 @RequiredArgsConstructor
+@Tag(name = "Memo", description = "Memo API")
 public class MemoController {
 
     private final MemoService memoService;
 
+    @Operation(summary = "메모 작성", description = "메모 작성 API")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "메모 작성 성공")
+    })
     @PostMapping
     public Response<CreateMemoResponse> createMemo(@CurrentUser User user,
         @Valid @RequestBody CreateMemoRequest request) {
